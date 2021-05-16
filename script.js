5ffdd4e9481c0038e267669172fa8a41c3d77839
dDigitalDev <dDigitalDev@gmail.com>
2021-05-16 19:10:00 +0200

    navlink on scroll

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const mainFn = () => {
   } else {
     navbar.classList.remove("sticky");
   }
-
+/** 
   sections.forEach((section, i) => {
     if (window.pageYOffset >= section.offsetTop - 10) {
       navbarLinks.forEach((navbarLink) => {
@@ -23,7 +23,7 @@ const mainFn = () => {
       });
       navbarLinks[i].classList.add("change");
     }
-  });
+  }); */
 
   if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
     document.querySelectorAll(".progress-percent").forEach((el, i) => {
